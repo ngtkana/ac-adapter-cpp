c68cde30c7d43ed18edf8320c743a71f8bf39332
ngtkana <ngtkana@gmail.com>
2020-05-12 20:04:26 +0900

    add cstdint2

---
+++ others/cstdint2.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cstdint>
+
+using i32 = std::int_least32_t;
+using i64 = std::int_least64_t;
+using u32 = std::uint_least32_t;
+using u64 = std::uint_least64_t;
+using usize = std::size_t;
